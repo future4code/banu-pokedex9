bb9661551add45f3ef5863e0ec88cbaf7ab2029d
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2021-11-19 00:20:13 -0300

    adciona global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './styles/GlobalStyles';
 
 import { theme } from './themes';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <h1>Pokedex</h1>
     </ThemeProvider>
   );
